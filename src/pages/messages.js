7de98f759078ebf4e570195c61e4b1abac3c8db6
NashibJr <nashibjr@gmail.com>
2023-04-29 07:37:33 -0700

    Adding and styling the message UI

--- src/pages/messages.js
+++ src/pages/messages.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Links from "../components/links";
+import "../styles/messages.css";
 
 const Messages = () => {
   return (
-    <div>
+    <div className="messages-content">
       <div>
         <Links />
+      </div>
+      <div className="messages">
+        <header>
+          <p>David De gea</p>
+        </header>
+        <main></main>
+        <footer>
+          <form>
+            <input type="text" placeholder="Type message..." name="message" />
+            <input type="button" value="send" />
+          </form>
+        </footer>
       </div>
     </div>
   );
